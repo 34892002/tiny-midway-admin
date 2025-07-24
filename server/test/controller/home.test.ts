02037984985e9c6b1e80ff3b7addf22d0d2b7789
ggyy <34892002@qq.com>
2025-07-24 15:37:45 +0800

    fix: 项目配置、单元测试

--- server/test/controller/home.test.ts
+++ server/test/controller/home.test.ts
@@ -12,7 +12,10 @@ describe('test/controller/home.test.ts', () => {
 
     // use expect by jest
     expect(result.status).toBe(200);
-    expect(result.text).toBe('Hello Midwayjs!');
+    const responseBody = JSON.parse(result.text);
+    expect(responseBody.code).toBe(0);
+    expect(responseBody.message).toBe('OK');
+    expect(responseBody.data).toHaveProperty('msg', 'Hello, MidwayJS + Casbin + Prisma = ❤️');
 
     // close app
     await close(app);
