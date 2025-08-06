7ab2ab0affc22db0345feb1e15af0ce05e3437eb
ggyy <34892002@qq.com>
2025-08-06 11:59:26 +0800

    fix: 重复code码

--- server/src/error/admin.error.ts
+++ server/src/error/admin.error.ts
@@ -1,3 +1,8 @@
+// - 10000-10999: 认证和授权相关错误
+// - 11000-11999: 用户数据相关错误
+// - 12000-12999: 业务逻辑错误
+// - 13000-13999: 系统和配置错误
+
 export const AdminErrorEnum = {
   CAPTCHA_ERROR: {
     code: 10003,
@@ -7,10 +12,6 @@ export const AdminErrorEnum = {
     code: 10002,
     error: '用户名密码错误!',
   },
-  DICT_NOT_DATA: {
-    code: 10003,
-    error: '未找到字典!',
-  },
   BAD_USER_DATA: {
     code: 11007,
     error: '用户数据异常!',
@@ -19,8 +20,13 @@ export const AdminErrorEnum = {
     code: 11008,
     error: '用户数据已更新',
   },
+  DICT_NOT_DATA: {
+    code: 13000,
+    error: '未找到字典!',
+  },
 };
 
+// 目前技术收益不大，没有使用
 export class CustomError extends Error {
   code: number | string;
   constructor(message: string, code: number) {
@@ -29,6 +35,7 @@ export class CustomError extends Error {
   }
 }
 
+// 目前技术收益不大，没有使用
 export class CaptchaError extends CustomError {
   constructor() {
     super(AdminErrorEnum.CAPTCHA_ERROR.error, AdminErrorEnum.CAPTCHA_ERROR.code);
